2fcba9142f98305f2ee858b8b36f3ccefbc2d0e4
Biraja prasad Swain <birajaprasadswain4@gmail.com>
2026-06-29 21:14:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 resource "aws_instance" "demo" {
   ami           = "ami-08f44e8eca9095668"
-  instance_type = "t2.micro"{
-   Name = "self-host" 
-}
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "self-host"
+  }
 }
